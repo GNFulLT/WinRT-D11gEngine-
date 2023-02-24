6351772635c20b270d58ebcfc445cd41052e6252
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-02-24 14:02:46 +0300

    Added to graphic

--- VulkanGameEngine/graphic/graphic_api.h
+++ VulkanGameEngine/graphic/graphic_api.h
@@ -1,10 +1,13 @@
 #ifndef GRAPHIC_API_H
 #define GRAPHIC_API_H
 
+#include <unordered_map>
+#include "../core/serialize/serializable_enum.h"
 
 enum GRAPHIC_API
 {
 	GRAPHIC_API_VULKAN = 0, 
+	GRAPHIC_API_DIRECTX_12,
 	GRAPHIC_API_DIRECTX_11
 };
 
